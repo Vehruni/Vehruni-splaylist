cef6d76d58bed77ea8902cd0c709afde1c6623d5
Vehruni Judistry <rema2judistry@gmail.com>
2019-01-15 23:22:56 +0000

    things

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ['Chunky','Shenyeng Anthem','Wow','Feel this moment'];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+ songs.forEach(function(Songlist){
+      
+});
 
+ 
+
+var imagesurls=["https://i.ytimg.com/vi/oacaq_1TkMU/maxresdefault.jpg", "https://images.genius.com/deecdf35caf0f60a9c0d1bb48c3ee3e1.800x800x1.jpg", "https://i.ytimg.com/vi/3fhbxflfsNs/hqdefault.jpg"  ]
+
+var artists=['Bruno Mars', 'Sheensea', 'Post Malone', 'Pitbull' ]
+ artists.forEach(function(Artistlist){
+      
+});
+
+var songlengths= [  ]
+ songlengths.forEach(function(Lengthslist){
+      
+});
+
+var links= ["https://www.youtube.com/watch?v=oacaq_1TkMU",'https://www.youtube.com/watch?v=73SbmC99zFc' , "https://www.youtube.com/watch?v=D0i23wZ0IeE ", "https://www.youtube.com/watch?v=r90Wwl2MPq0"   ]
+ links.forEach(function(Linkslist){
+      
+});
 
 
 function displaySongInfo(){
